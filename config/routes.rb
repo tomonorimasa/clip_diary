c641881e4bb69475484a60d2b4e1500b7be902d0
tomonorimasa <krho.ashra2@gmail.com>
2023-11-02 15:53:22 +0900

    メールアドレスリセット機能実装（開発）

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#top'
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
@@ -15,4 +16,5 @@ Rails.application.routes.draw do
   end
   resources :likes, only: %i[create destroy]
   resource :profile, only: %i[show edit update]
+  resources :password_resets, only: %i[new create edit update]
 end
